a9764b787e3dc25ed9f9ae1ad73934000b0ddf45
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-25 23:00:00 +0200

    Fetching the feeds from our json file

--- providentia/core/sources/rss/fetchers.py
+++ providentia/core/sources/rss/fetchers.py
@@ -1,4 +1,5 @@
 from sources import DataSource
+import feedparser
 
 class Fetcher(object):
     __data_adapter = None
@@ -7,7 +8,15 @@ class Fetcher(object):
         self.__data_adapter = DataSource.get_instance()
 
     def fetch_documents(self):
+        feed_list = []
         rss_list = self.__data_adapter.fetch_rss()
-        return rss_list
+        for rss in rss_list:
+            rss_url = rss.get('url', None)
+            rss_title = rss.get('title', '-')
+            if rss_url is not None:
+                feed = feedparser.parse(rss_url)
+                feed_list.append({'title':rss_title, "feed": feed})
+
+        return feed_list
 
 
