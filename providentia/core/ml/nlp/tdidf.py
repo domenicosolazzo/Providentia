70eeab3961ffa959624b92fff8b43a54454b16d6
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-26 14:06:06 +0100

    Refactoring inside the TdIdf class; Added term frequency function

--- providentia/core/ml/nlp/tdidf.py
+++ providentia/core/ml/nlp/tdidf.py
@@ -1,3 +1,5 @@
+import math
+from operator import itemgetter
 class TdIdf(object):
     word = ""
     document = ""
@@ -8,27 +10,36 @@ class TdIdf(object):
         self.document = document
         self.document_list = documentList
 
-    def frequency(self, document):
+    def frequency(self, word, document):
         """
         Frequency of a word in a given document
         :return: Frequency of a word in a given document
         """
-        return document.count(self.word)
+        return document.count(word)
 
-    def wordcount(self):
+    def wordcount(self, document):
         """
         Word count in a document
         :return: Number of characters in a document
         """
-        return len(self.document)
+        return len(document)
 
-    def numDocsContainingWord(self):
+    def numDocsContainingWord(self, word, document_list):
         """
         Number of document containing a given word
         :return: The number of documents containing a given word
         """
         count = 0
-        for doc in self.document_list:
-            if self.frequency(doc) > 0:
+        for doc in document_list:
+            if self.frequency(word, doc) > 0:
               count += 1
         return count
+
+    def tf(self, word, document):
+        """
+        Term frequency of a word in given document
+        :param word: A given word
+        :param document: A given document
+        :return: term frequency
+        """
+        return (self.frequency(word,document) / float(self.wordCount(document)))
--- tests/core/ml/nlp/tdidf_tests.py
+++ tests/core/ml/nlp/tdidf_tests.py
@@ -5,17 +5,17 @@ def test_frequency_of_a_word():
     document = "abc abc abc ciao I am Domenico"
     word = "abc"
     tdidf = TdIdf(word, document, [])
-    result = tdidf.frequency(document)
+    result = tdidf.frequency(word, document)
     assert_equal(3, result)
 
 def test_word_count():
     document = "I am Domenico"
     tdidf = TdIdf("", document, [])
-    result = tdidf.wordcount()
+    result = tdidf.wordcount(document)
     assert_equal(13, result)
 
 def test_num_docs_containing_word():
     documentList = ["I am Domenico", "Let's watch tv", "Domenico watches tv"]
     tdidf = TdIdf("Domenico", "", documentList)
-    result = tdidf.numDocsContainingWord()
+    result = tdidf.numDocsContainingWord("Domenico",documentList)
     assert_equal(2, result)
