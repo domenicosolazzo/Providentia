736ecb6d7db06be82711f70649085dd4c1f602bf
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-26 12:59:50 +0100

    Added class TdIdf

--- providentia/core/ml/nlp/tdidf.py
+++ providentia/core/ml/nlp/tdidf.py
@@ -1,8 +1,12 @@
-def freq(word, document):
-    """
-    Frequency of a word in a given document
-    :param word: The word
-    :param document: The document
-    :return: Frequency of a word in a given document
-    """
-    return document.count(word)
+class TdIdf(object):
+    def __init__(self, word, document, documentList):
+        pass
+
+    def freq(word, document):
+        """
+        Frequency of a word in a given document
+        :param word: The word
+        :param document: The document
+        :return: Frequency of a word in a given document
+        """
+        return document.count(word)
