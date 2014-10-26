7c77335e194a034928c99f04bd17d70d483f7327
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-26 12:53:52 +0100

    Fixed some unit tests

--- providentia/core/ml/nlp/models/document.py
+++ providentia/core/ml/nlp/models/document.py
@@ -3,5 +3,9 @@ class BasicDocument(object):
         pass
 
 class FeedDocument(object):
-    def __init__(self):
-        pass
+    title = ''
+    description = ''
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
--- tests/core/sources/rss/fetchers_tests.py
+++ tests/core/sources/rss/fetchers_tests.py
@@ -3,5 +3,5 @@ from providentia.core.sources.rss.fetchers import Fetcher
 
 def test_fetcher():
     f = Fetcher()
-    document_list = f.fetch_documents()
+    document_list = f.fetch_feeds()
     assert_true(document_list is not None)
