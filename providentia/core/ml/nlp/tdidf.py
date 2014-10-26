8e89d890aa7c6ce85a5c73d607d8cf8284f68241
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-26 12:57:23 +0100

    Frequency of a word in a given document

---
+++ providentia/core/ml/nlp/tdidf.py
@@ -0,0 +1,8 @@
+def freq(word, document):
+    """
+    Frequency of a word in a given document
+    :param word: The word
+    :param document: The document
+    :return: Frequency of a word in a given document
+    """
+    return document.count(word)
